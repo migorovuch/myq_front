d5dc033933adfd5ea0d7827fce2e086d211b74c9
Maxym Humenetskyi <migorovuch@gmail.com>
2021-09-25 13:34:55 +0300

    upload logo message, fix company view logo

--- app/src/views/views/company/CompanyView.vue
+++ app/src/views/views/company/CompanyView.vue
@@ -4,17 +4,17 @@
       <div class="row">
         <div class="col-2" v-if="getCompany().logo">
           <div class="comapny-logo">
-            <img :src="'http://q.localhost/media/'+getCompany().logo" alt="Company logo">
+            <img :src="logoPreview" alt="Company logo">
           </div>
         </div>
         <div class="col">
           <h1>{{getCompany().name}}</h1>
         </div>
       </div>
-      <div class="row">
-        <div class="col form-group" v-if="getCompany().phone"><a :href="'tel:'+getCompany().phone">{{ getCompany().phone }}</a></div>
-        <div class="col form-group" v-if="getCompany().email"><a :href="'mailto:'+getCompany().email">{{ getCompany().email }}</a></div>
-        <div class="col form-group" v-if="getCompany().address">
+      <div class="row pt-3 pb-3">
+        <div class="col" v-if="getCompany().phone"><a :href="'tel:'+getCompany().phone">{{ getCompany().phone }}</a></div>
+        <div class="col" v-if="getCompany().email"><a :href="'mailto:'+getCompany().email">{{ getCompany().email }}</a></div>
+        <div class="col" v-if="getCompany().address">
           <a :href="getCompany().addressLink" v-if="getCompany().addressLink" target="_blank">{{ getCompany().address }}</a>
           <span v-else>{{ getCompany().address }}</span>
         </div>
@@ -73,7 +73,8 @@ export default {
     return {
       selectedSchedule: null,
       bookingStart: null,
-      queryTime: null
+      queryTime: null,
+      logoPreview: null
     };
   },
   created() {
@@ -89,6 +90,7 @@ export default {
             }
           }
         });
+        this.logoPreview = process.env.VUE_APP_API_URL.replaceAll('\'', '') + '/media/' + this.getCompany().logo;
       }
     });
   },
