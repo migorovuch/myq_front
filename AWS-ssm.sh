0df15317915f5ef7d9c47c0ef851420978ff6f73
Maksym Humenetskyi <migorovuch@gmail.com>
2023-01-02 23:17:49 +0200

    AWS buldspec

---
+++ AWS-ssm.sh
@@ -0,0 +1,32 @@
+# Add environment variables to AWS Parameter Store on the same path prefix for the same project
+# To get the environment variables for a project, get the variables by path
+# Convert it to a .env file format
+# env1=abc
+# env2=def
+# separated by the newline character
+# and write to a .env file
+
+myService=/myq/front/prod
+# Get variables from SSM and chop off the service name from the variable names (/myservice_name/stage_name/PORT to PORT) and write to a JSON file
+# (env.json)
+# [   {
+#         "Name": "RANDOM_NUMBER",
+#         "Value": "99"
+#     },
+#     {
+#         "Name": "RANDOM_SECRET",
+#         "Value": "324546JYHBERGYTUDHGsANEFRGW5cerhuwdfshd"
+#     }
+# ]
+aws ssm get-parameters-by-path --profile maxsprinter_myq --with-decryption --path $myService --query "Parameters[*].{Name:Name,Value:Value}" | sed 's,'"$myService/"',,' > env.json
+
+
+# Convert JSON array to variable statements using jq (https://stedolan.github.io/jq/) command line JSON utility tool
+# (.env)
+# RANDOM_NUMBER=99
+# RANDOM_SECRET=324546JYHBERGYTUDHGsANEFRGW5cerhuwdfshd
+
+#WITH QUOTES
+#jq -jr '.[]|.Name,"='"'"'",.Value,"'"'"'\n"' < env.json >> .env.local
+jq -jr '.[]|.Name,"=",.Value,"\n"' < env.json >> .env.local
+rm env.json
