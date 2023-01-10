b39082f66a4765817aebd4fb35da340cba179069
Maksym Humenetskyi <migorovuch@gmail.com>
2023-01-10 23:40:28 +0200

    change relative path

--- app/AWS-ssm.sh
+++ app/AWS-ssm.sh
@@ -18,7 +18,7 @@ myService=/myq/front/prod
 #         "Value": "324546JYHBERGYTUDHGsANEFRGW5cerhuwdfshd"
 #     }
 # ]
-aws ssm get-parameters-by-path --with-decryption --path $myService --query "Parameters[*].{Name:Name,Value:Value}" | sed 's,'"$myService/"',,' > ./env.json
+aws ssm get-parameters-by-path --with-decryption --path $myService --query "Parameters[*].{Name:Name,Value:Value}" | sed 's,'"$myService/"',,' > ./app/env.json
 
 
 # Convert JSON array to variable statements using jq (https://stedolan.github.io/jq/) command line JSON utility tool
@@ -28,6 +28,5 @@ aws ssm get-parameters-by-path --with-decryption --path $myService --query "Para
 
 #WITH QUOTES
 #jq -jr '.[]|.Name,"='"'"'",.Value,"'"'"'\n"' < env.json >> .env.local
-touch ./.env.local
-jq -jr '.[]|.Name,"=",.Value,"\n"' < ./env.json >> ./.env.local
-rm ./env.json
+jq -jr '.[]|.Name,"=",.Value,"\n"' < ./app/env.json >> ./app/.env.local
+rm ./app/env.json
